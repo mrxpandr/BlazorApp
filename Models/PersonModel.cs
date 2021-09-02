d60eabba7b74c70d2e19669fb6f742ac861c6006
Marvin Ebuenga <mrx.pandr@gmail.com>
2021-09-02 13:36:21 +0800

    Add project files.

---
+++ Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
